fee64d4801b9f2f847da8f43a05636229009a7da
Nezumi-sama <kryska007@yandex.ru>
2022-08-29 15:33:44 +0300

    First jproject_3_02

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        //Объявляем переменные
+        int price = 246;
+        int bonus;
+
+        //рассчет и вывод результатов
+        bonus = price / 20;
+        System.out.println("Начислено бонусных миль: " + bonus);
+
+    }
+}
